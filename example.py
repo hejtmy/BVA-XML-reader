3e55e159d77c19678d09a05ceb6c36de03abda16
hejtmy <hejtmy@gmail.com>
2019-04-01 18:52:44 +0200

    Adds preprocessing functions

--- example.py
+++ example.py
@@ -5,16 +5,17 @@ from functions.bva_xml_reader import read_xml_sync
 from functions.bva_xml_reader import read_xml_phases
 from functions.bva_sync_reader import read_sync_file
 from functions.bva_plotting import plot_triangle
+from functions.bva_preprocessors import preprocess_bva_data
 from functions.bva_preprocessors import add_rotation
 
 pd_bva = read_xml_bva("example_data/example.xml")
-pd_bva = add_rotation(pd_bva)
-
 plot_triangle(pd_bva, 1000)
 
-save_csv(pd_bva, "bva.csv")
+pd_bva2 = preprocess_bva_data(pd_bva)
+pd_bva = add_rotation(pd_bva)
 
-plt.plot(pd_bva.Point_x, pd_bva.Point_y)
+save_csv(pd_bva2, "bva.csv")
+save_csv(pd_bva, "bva_full.csv")
 
 pd_sync = read_sync_file("example_data/example.csv")
 save_csv(pd_sync, "sync.csv")
--- functions/bva_preprocessors.py
+++ functions/bva_preprocessors.py
@@ -1,19 +1,45 @@
 import pandas as pd
 import numpy as np
 
+# Wrapper around multiple preprocessing functions
+# Adds rotation
+# clears out positions
+def preprocess_bva_data(pd_bva):
+  pd_bva = clear_out_of_arena_positions(pd_bva)
+  pd_bva = add_rotation(pd_bva)
+  pd_bva = remove_unnecessary_columns(pd_bva)
+  pd_bva = rename_columns(pd_bva)
+  return(pd_bva)
+
 
 # Removes any points that are out of constraints of the bva and replaces then with NAs
 def clear_out_of_arena_positions(pd_bva):
-    pd_bva.Point_x[np.abs(pd_bva.Point_x) > 250] = np.nan
-    pd_bva.Point_y[np.abs(pd_bva.Point_y) > 250] = np.nan
-    return(pd_bva)
+  pd_bva.Point_x[np.abs(pd_bva.Point_x) > 250] = np.nan
+  pd_bva.Point_y[np.abs(pd_bva.Point_y) > 250] = np.nan
+  return(pd_bva)
 
 
 def add_rotation(pd_bva):
   x_cross, y_cross = calculate_perpendicular_cross(pd_bva.Left_x, pd_bva.Left_y, pd_bva.Right_x, pd_bva.Right_y, pd_bva.Front_x, pd_bva.Front_y)
   front_x, front_y = pd_bva.Front_x - x_cross, pd_bva.Front_y - y_cross
   zipped = list(zip(front_x, front_y))
-  pd_bva['rotation'] = angle_between(zipped, [(0,1)])
+  pd_bva['rotation_x'] = angle_between(zipped, [(0,1)])
+  return(pd_bva)
+
+
+# Removes columns used only for calculation of rotation
+def remove_unnecessary_columns(pd_bva, force=False):
+  #checks if rotation has been calculated
+  cols = ['Right_x', 'Right_y', 'Left_x', 'Left_y', 'Front_x', 'Front_y']
+  if 'rotation_x' not in pd_bva.columns:
+    Warning('You are deleting columns without calculating rotation first. set force to True if you want to really delete')
+  if 'rotation_x' in pd_bva.columns or force:
+    pd_bva = pd_bva.drop(cols, axis=1)
+  return(pd_bva)
+
+
+def rename_columns(pd_bva):
+  pd_bva = pd_bva.rename(columns={"Point_x":'position_x', 'Point_y':'position_y'})
   return(pd_bva)
 
 
@@ -35,11 +61,6 @@ def calculate_perpendicular_cross_classical(line1_x, line1_y, line2_x, line2_y, 
   return(cross_x, cross_y)
 
 
-def navr_output(pd_bva):
-  pd_navr_bva = pd_bva
-  return(pd_navr_bva)
-
-
 # p1 are lists of touples [(0,1),(1,0)]
 def angle_between(p1, p2):
   x, y = zip(*p1[::1])
@@ -48,4 +69,3 @@ def angle_between(p1, p2):
   ang2 = np.arctan2(x, y)
   degrees = np.rad2deg((ang1 - ang2) % (2 * np.pi))
   return(degrees)
-
