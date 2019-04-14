94499ce0992a49c8860564fa271248737efd9c0b
hejtmy <hejtmy@gmail.com>
2019-04-15 00:14:48 +0200

    Adds flake pylint

--- setup.py
+++ setup.py
@@ -15,19 +15,19 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/hejtmy/bva-reader",
     packages=setuptools.find_packages(),
-		install_requires=[
-			'pandas',
-			'numpy>=1.16.0',
-			'matplotlib',
-		],
+    install_requires=[
+      'pandas',
+      'numpy>=1.16.0',
+      'matplotlib',
+      ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-		entry_points={
-			'console_scripts':[
-				'bva-preprocess-xml = bvareader.commands:bva_preprocess_xml'
-			]
-		}
+      ],
+    entry_points={
+      'console_scripts': [
+        'bva-preprocess-xml = bvareader.commands:bva_preprocess_xml'
+        ]
+    }
 )
