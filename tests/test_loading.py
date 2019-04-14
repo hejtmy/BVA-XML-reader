0b949fada975abd0e0d7e6437aa0da1500afaa65
hejtmy <hejtmy@gmail.com>
2019-04-15 01:15:10 +0200

    Adds sync loading test

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -9,8 +9,8 @@ def bva_xml_data_path():
 
 
 @pytest.fixture
-def bva_xml_data_path():
-    return "tests/test_data/example_sync.xml"
+def sync_csv_data_path():
+    return "tests/test_data/example_sync.csv"
 
 
 def test_loading_bva_xml(bva_xml_data_path):
@@ -18,5 +18,6 @@ def test_loading_bva_xml(bva_xml_data_path):
     assert isinstance(pd_bva, pd.core.frame.DataFrame)
 
 
-def test_loading_sync_csv():
-    reader.read_sync_file("example_data/example.csv")
+def test_loading_sync_csv(sync_csv_data_path):
+    pd_sync = reader.read_sync_file(sync_csv_data_path)
+    assert isinstance(pd_sync, pd.core.frame.DataFrame)
