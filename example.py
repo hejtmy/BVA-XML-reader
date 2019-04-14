5d54e1499d9370a55ab572db033150c64e1a943b
hejtmy <hejtmy@gmail.com>
2019-04-15 01:12:34 +0200

    Whitelists test data

--- example.py
+++ example.py
@@ -1,19 +1,15 @@
-from bvareader.reader import read_xml_bva
-from bvareader.reader import read_xml_sync
-from bvareader.reader import read_sync_file
-from bvareader.plotter import plot_triangle
-from bvareader.reader import save_csv
-from bvareader.preprocessing import preprocess_bva_data
-from bvareader.preprocessing import add_rotation
+from bvareader import reader
+from bvareader import plotter 
+from bvareader import preprocessing
 
-pd_bva = read_xml_bva("example_data/example.xml")
-plot_triangle(pd_bva, 1000)
+pd_bva = reader.read_xml_bva("example_data/example.xml")
+plotter.plot_triangle(pd_bva, 1000)
 
-pd_bva2 = preprocess_bva_data(pd_bva)
-pd_bva = add_rotation(pd_bva)
+pd_bva2 = preprocessing.preprocess_bva_data(pd_bva)
+pd_bva = preprocessing.add_rotation(pd_bva)
 
-save_csv(pd_bva2, "bva.csv")
-save_csv(pd_bva, "bva_full.csv")
+reader.save_csv(pd_bva2, "bva.csv")
+reader.save_csv(pd_bva, "bva_full.csv")
 
-pd_sync = read_sync_file("example_data/example.csv")
-save_csv(pd_sync, "sync.csv")
+pd_sync = reader.read_sync_file("example_data/example.csv")
+reader.save_csv(pd_sync, "sync.csv")
--- tests/test_loading.py
+++ tests/test_loading.py
@@ -5,9 +5,18 @@ import pandas as pd
 
 @pytest.fixture
 def bva_xml_data_path():
-    return "tests/test_data/example.xml"
+    return "tests/test_data/example_bva.xml"
+
+
+@pytest.fixture
+def bva_xml_data_path():
+    return "tests/test_data/example_sync.xml"
 
 
 def test_loading_bva_xml(bva_xml_data_path):
     pd_bva = reader.read_xml_bva(bva_xml_data_path)
     assert isinstance(pd_bva, pd.core.frame.DataFrame)
+
+
+def test_loading_sync_csv():
+    reader.read_sync_file("example_data/example.csv")
